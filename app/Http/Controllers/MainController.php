9e614569ab521110a599680b61c02f6f5f3b7134
Vale <oirelav95@gmail.com>
2023-02-02 15:26:51 +0100

    creo controller MainController

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Saint;
+
+class MainController extends Controller
+{
+    public function home() {
+        $saints = Saint::all();
+
+        $data = [
+            'saints' => $saints
+        ];
+
+        return view('pages.home', $data);
+    }
+
+    public function show($id) {
+
+        $saint = Saint::find($id);
+
+        $data = [
+            'saint' => $saint
+        ];
+
+        return view('pages.saint', $data);
+
+
+
+    }
+}
