9bc06311b590648310e9bb4a812bc1de874a31a9
Vale <oirelav95@gmail.com>
2023-02-02 15:28:12 +0100

    creo file home.blade.php

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main-layout')
+
+@section('content')
+
+    <h1>
+        Santi
+    </h1>
+
+    <ul>
+        @foreach ($saints as $saint)
+
+            <li>
+                <a href="/saint/{{$saint -> id}}">{{$saint->name}}</a> - Numero miracoli: {{$saint->number_of_miracles}}
+            </li>
+        
+        @endforeach 
+    </ul>
+    
+@endsection
