763838b4a940bef4c220e2e9b8d1c4763102734f
Vale <oirelav95@gmail.com>
2023-02-03 15:34:47 +0100

    aggiungo collegamento a nuova pagina per creare elemento

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,11 +6,15 @@
         Santi
     </h1>
 
+    <h2>
+        <a href="{{ route('saint.create') }}">Create new saint</a>
+    </h2>
+
     <ul>
         @foreach ($saints as $saint)
 
             <li>
-                <a href="{{ route('show', ['id' => $saint -> id]) }}">{{$saint -> name}}</a> - Numero miracoli: {{$saint -> number_of_miracles}}  - 
+                <a href="{{ route('saint.show', ['id' => $saint -> id]) }}">{{$saint -> name}}</a> - Numero miracoli: {{$saint -> number_of_miracles}}  - 
                 <a href="{{ route('saint.destroy', ['id' => $saint -> id]) }}">Delete</a>
             </li>
         
