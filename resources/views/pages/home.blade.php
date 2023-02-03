5913c83605dd6614b80807334a49717ce7fefbb0
Vale <oirelav95@gmail.com>
2023-02-03 13:47:09 +0100

    aggiungo tag a per eliminare elemento

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,7 +10,8 @@
         @foreach ($saints as $saint)
 
             <li>
-                <a href="{{ route('show', ['id' => $saint -> id]) }}">{{$saint -> name}}</a> - Numero miracoli: {{$saint -> number_of_miracles}}
+                <a href="{{ route('show', ['id' => $saint -> id]) }}">{{$saint -> name}}</a> - Numero miracoli: {{$saint -> number_of_miracles}}  - 
+                <a href="{{ route('saint.destroy', ['id' => $saint -> id]) }}">Delete</a>
             </li>
         
         @endforeach 
